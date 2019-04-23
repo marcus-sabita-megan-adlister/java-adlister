82d1c40e9cb8363f0ffd77acd0439a8c14252106
megansimmons <simmons.megan584@gmail.com>
2019-04-23 15:01:26 -0500

    database tables created

--- migration.sql
+++ migration.sql
@@ -1,7 +1,8 @@
+DROP DATABASE IF EXISTS adlister_db;
+CREATE DATABASE adlister_db;
+
 USE adlister_db;
 
-DROP TABLE IF EXISTS ads;
-DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -16,7 +17,23 @@ CREATE TABLE ads (
     user_id INT UNSIGNED NOT NULL,
     title VARCHAR(240) NOT NULL,
     description TEXT NOT NULL,
+    image TEXT,
+    price FLOAT UNSIGNED NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (user_id) REFERENCES users(id)
         ON DELETE CASCADE
+);
+
+
+CREATE TABLE user_ads(
+     ad_id INT UNSIGNED NOT NULL,
+     user_id INT UNSIGNED NOT NULL,
+     FOREIGN KEY (user_id) REFERENCES users(id),
+     FOREIGN KEY (ad_id) REFERENCES ads(id)
+);
+
+CREATE TABLE cat_ads(
+    category VARCHAR(255) NOT NULL,
+    ad_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (ad_id) REFERENCES ads(id)
 );
