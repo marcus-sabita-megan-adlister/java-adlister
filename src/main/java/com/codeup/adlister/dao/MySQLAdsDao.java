9552382cc5ff561f9f92bb117696e52c291e631f
Sabita-Regmi <sabitaregmi3291@gmail.com>
2019-04-24 10:32:11 -0500

    completed work on signUpButton branch

--- src/main/java/com/codeup/adlister/dao/MySQLAdsDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLAdsDao.java
@@ -60,7 +60,8 @@ public class MySQLAdsDao implements Ads {
             rs.getLong("id"),
             rs.getLong("user_id"),
             rs.getString("title"),
-            rs.getString("description")
+            rs.getString("description"),
+                rs.getString("price")
         );
     }
 
